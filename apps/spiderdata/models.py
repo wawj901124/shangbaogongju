8d4afa4aaadb7e71d4b07462a78f11ea033b4814
wawj900805 <410287958@qq.com>
2020-04-13 05:09:53 +0800

    gengxin

--- apps/spiderdata/models.py
+++ apps/spiderdata/models.py
@@ -101,6 +101,7 @@ class SpiderData(models.Model):
     # img_width = models.CharField(max_length=100, default=75, null=True, blank=True,verbose_name=u"封面图宽度")
     # front_cover_img = models.ImageField(upload_to="report/%Y%m/screenshots/" , null=True, blank=True,verbose_name=u"封面图片", height_field='img_height',width_field='img_width',max_length=2000)
     front_cover_img = models.CharField(max_length=1500, null=True, blank=True,verbose_name=u"封面图片")
+    prenum = models.CharField(max_length=100, default="", null=True, blank=True, verbose_name=u"编号")
 
     #对于ManyToManyField，没有null参数，如果加上会报警告如：spiderdata.SpiderData.genre: (fields.W340) null has no effect on ManyToManyField.
     genre = models.ManyToManyField(SpiderGenre,default="", blank=True,verbose_name=u"类别")
@@ -109,7 +110,6 @@ class SpiderData(models.Model):
     studio = models.ManyToManyField(SpiderStudio,default="",blank=True,verbose_name=u"制作商")
     director = models.ManyToManyField(SpiderDirector,default="",blank=True,verbose_name=u"导演")
 
-    prenum = models.CharField(max_length=100, default="", null=True, blank=True, verbose_name=u"编号")
     write_user = models.ForeignKey(User, null=True, blank=True, verbose_name=u"用户名", on_delete=models.PROTECT)
     add_time = models.DateTimeField(null=True, blank=True,auto_now_add=True,
                                     verbose_name=u"添加时间")  # datetime.now记录实例化时间，datetime.now()记录模型创建时间,auto_now_add=True是指定在数据新增时, 自动写入时间
