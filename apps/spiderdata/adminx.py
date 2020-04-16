765922af4a6dc9d642813d8c32bf36e5516b2c55
wawj900805 <410287958@qq.com>
2020-04-17 05:27:32 +0800

    gengxin

--- apps/spiderdata/adminx.py
+++ apps/spiderdata/adminx.py
@@ -32,7 +32,7 @@ class SpiderDataAdmin(object):
     # readonly_fields = ziduan  # 设置某些字段为只为可读  #设置了readonly_fields，再设置exclude，exclude对该字段无效，
     # exclude = ['case_step']  # 设置某些字段为不显示，即隐藏  #readonly_fields和exclude设置会有冲突
     # inlines = [TestCaseInline]  # inlines配和TestCaseInline使用，可以直接在项目页面添加测试用例#只能做一层嵌套，不能进行两层嵌套
-    # list_editable = ziduan   # 可以在列表页对字段进行编辑
+    list_editable = ['is_love','is_check',]  # 可以在列表页对字段进行编辑
     refresh_times = [3, 5]  # 对列表页进行定时刷新,配置了3秒和5秒，可以从中选择一个
     list_per_page = 50   #每页设置50条数据，默认每页展示100条数据
     # fk_fields = ['test_project_id',]  #设置显示外键字段，未生效
