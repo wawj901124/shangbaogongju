0143b2db363942fa64d4b5e4c0e7184c0983f2fc
wawj900805 <410287958@qq.com>
2020-04-19 17:31:44 +0800

    gengxin

--- apps/spiderdata/views.py
+++ apps/spiderdata/views.py
@@ -22,8 +22,26 @@ class SpiderHMChapterImageView(View):  # 继承View
             spiderhmchapterdata = SpiderHMChapterData.objects.get(id=int(spiderhmchapterdata_id))
             spiderhmchapterimage_list = SpiderHMChapterImage.objects.filter(spiderhmchapterdata_id=int(spiderhmchapterdata_id)).order_by("chapter_image_num")  # 获取实例对象
             is_with_relevance = 1
+
+            spiderhmchapterdata_list = SpiderHMChapterData.objects.filter(spiderhmbook_id=spiderhmchapterdata.spiderhmbook_id).order_by("chapter_num")
+            current_chapter_num = spiderhmchapterdata.chapter_num
+            chapter_num_list = []
+            for i in spiderhmchapterdata_list:
+                chapter_num_list.append(i.chapter_num)
+            print("chapter_num_list:")
+            print(chapter_num_list)
+            current_chapter_num_index = chapter_num_list.index(current_chapter_num)
+            chapter_num_list_len = len(chapter_num_list)
+            if current_chapter_num_index == chapter_num_list_len-1:
+                next_chapter_num_index = current_chapter_num_index
+            else:
+                next_chapter_num_index = current_chapter_num_index+1
+            next_chapter_num = chapter_num_list[next_chapter_num_index]
+            next_spiderhmchapterdata = SpiderHMChapterData.objects.get(chapter_num=next_chapter_num)
+
             return render(request, "spiderhmchapterimage/SpiderHMChapterImage.html",
                           {"spiderhmchapterdata":  spiderhmchapterdata,
+                           "nextspiderhmchapterdata":next_spiderhmchapterdata,
                            "spiderhmchapterimage_list":  spiderhmchapterimage_list,
                            "django_server_yuming": DJANGO_SERVER_YUMING,
                            "is_withRelevance": is_with_relevance,
